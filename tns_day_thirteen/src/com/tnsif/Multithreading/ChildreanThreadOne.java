4c7fc529baaaea43cf2b7f9bf63110697a758dca
yashyogeshwarnandre <yashyogeshwarnandre@localhost>
2023-10-05 23:57:28 +0530

    Thirteen Commit

---
+++ tns_day_thirteen/src/com/tnsif/Multithreading/ChildreanThreadOne.java
@@ -0,0 +1,9 @@
+package com.tnsif.Multithreading;
+
+public class ChildreanThreadOne  extends Thread{
+	public void run()
+	{
+		System.out.println("Factorial of no is:"+operation.getFact(4));
+	}
+
+}
