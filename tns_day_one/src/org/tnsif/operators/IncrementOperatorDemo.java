5c671e3074845a96cabea17aebf6e0ffb0fbb6d9
yashyogeshwarnandre <yashyogeshwarnandre@localhost>
2023-10-05 23:55:35 +0530

    Ninth Commit

--- tns_day_one/src/org/tnsif/operators/IncrementOperatorDemo.java
+++ tns_day_one/src/org/tnsif/operators/IncrementOperatorDemo.java
@@ -4,7 +4,7 @@ public class IncrementOperatorDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=23,y=6;
+		int x=11,y=15;
 		int res=x++ + ++y;
 		System.out.println("Result"+res);
 		System.out.println("x :"+x);
